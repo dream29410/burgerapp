e537853fdb3eb5d022017c13f64fa1fac5ffaf9a
dream29410 <dream29410@gmail.com>
2022-08-16 16:05:05 +0530

    rest some changes

---
+++ burgerapp/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue ,
+      ),
+      home: HomeScreen(),
+    );
+
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+           height: MediaQuery.of(context).size.height,
+           ;
+      ),
+      ),
+    );
+  }
+}
+
+
